87cc932510bba9d79444beaef31698ec8ffb2499
JaiVon <frederickjaivon@yahoo.com>
2022-10-04 17:04:29 -0400

    added user

--- back-end/app/controllers/application_controller.rb
+++ back-end/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
-class ApplicationController < ActionController::Base
+module Api
+    class ApplicationController < ActionController::Base
+        before_action :authenticate
+        
+        private
+        def authenticate
+            authenticate_or_request_with_http_token do |token, _options|
+              User.find_by(token: token)
+            end
+        end
+
+        def current_user
+            @current_user ||= authenticate
+        end
+    end
 end
