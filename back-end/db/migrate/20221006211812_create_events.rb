6a6e8da567b9148129c604833c3d16fe1a8396f6
JaiVon <frederickjaivon@yahoo.com>
2022-10-13 20:03:13 -0400

    changes

--- back-end/db/migrate/20221006211812_create_events.rb
+++ back-end/db/migrate/20221006211812_create_events.rb
@@ -6,6 +6,7 @@ class CreateEvents < ActiveRecord::Migration[7.0]
       t.datetime :ends
       t.string :details
       t.string :location
+      t.belongs_to :event_month, foreign_key: true
 
       t.timestamps
     end
