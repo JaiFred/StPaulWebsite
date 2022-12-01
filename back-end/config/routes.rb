cd95636dc769445f373879e19dce46f5730415f5
JaiVon <frederickjaivon@yahoo.com>
2022-11-30 21:00:29 -0500

    WIP devise install and configuration

--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
       
 
     namespace :api do       
@@ -31,6 +36,5 @@ Rails.application.routes.draw do
     
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Defines the root path route ("/")  
 end
--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_040738) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_013956) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -82,11 +82,20 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_040738) do
     t.string "last_name"
     t.string "username"
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.string "stripe_customer_id"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
