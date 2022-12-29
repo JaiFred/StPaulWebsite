99e2e36f85a4110d98a50d7fbe4de7f44bd4dd56
JaiVon <frederickjaivon@yahoo.com>
2022-12-29 00:28:06 -0500

    Added a hide facebook button command

--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_16_023132) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_29_044906) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_16_023132) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "button_visible_configs", force: :cascade do |t|
+    t.boolean "facebook", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "documents", force: :cascade do |t|
