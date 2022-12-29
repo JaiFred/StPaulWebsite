ef0074969b2963403977350c39d3a3b162b2fa7f
JaiVon <frederickjaivon@yahoo.com>
2022-12-28 22:04:08 -0500

    added nearest payment date to subscription page information

--- front-end/src/Footer.js
+++ front-end/src/Footer.js
@@ -3,24 +3,45 @@ import { Link } from "react-router-dom";
 
 import ContactUsPage from "./ContactUsPage"
 
-function Footer(){
+function Footer({ currentUser }){
 
     return(
         <div className="footer">
-            {/* <button className='contact-us-btn' type='button'><a href='mailto:stpaul23009@gmail.com
-            '>Contact Us</a></button> */}
-            <button>
+            {(currentUser?.admin === true || currentUser?.user?.admin === true) ? (
+                <div>
+                    <button>
                 <div className="contact-us-button">
                     <Link to='/contact_us'>Contact Us</Link>
                 </div>
             </button>
 
-            <link 
-            rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+            <link
+            rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
             </link>
             <a href="#" class="fa fa-facebook"></a>
-                
+            <button>
+                <div className="facebook-link-off">
+                    Turn link off
+                </div>
+            </button>
+                </div>
+            ) : (
+                <div>
+                    <button>
+                        <div className="contact-us-button">
+                            <Link to='/contact_us'>Contact Us</Link>
+                        </div>
+                    </button>
+
+                    <link
+                    rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                    </link>
+                    <a href="#" class="fa fa-facebook"></a>
+                </div>
+            )}
+            {/* <button className='contact-us-btn' type='button'><a href='mailto:stpaul23009@gmail.com
+            '>Contact Us</a></button> */}
         </div>
     )
 
