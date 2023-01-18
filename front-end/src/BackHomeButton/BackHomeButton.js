c65fbdc50dc379ef81e8081a77d0d4e7e427beef
JaiVon <frederickjaivon@yahoo.com>
2023-01-17 19:19:06 -0500

    working on about page - made photos circular, added fade to the bakground, made banner behind pictures and organized text below title

--- front-end/src/BackHomeButton/BackHomeButton.js
+++ front-end/src/BackHomeButton/BackHomeButton.js
@@ -1,9 +1,10 @@
 //Hooks
+import classNames from "classnames";
 import { Link } from "react-router-dom";
 
 import './BackHomeButton.scss';
 
-export const BackHomeButton = ({}) => 
-<div className="back-home">
+export const BackHomeButton = ({ className }) => 
+<div className={classNames("back-home", className)}>
     <Link to='/' className="back-home-button">Back Home</Link>
 </div>
