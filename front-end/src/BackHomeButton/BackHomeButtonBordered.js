f90d051ec7007b7e9947485acab441b5fa3e4089
JaiVon <frederickjaivon@yahoo.com>
2023-01-21 20:50:55 -0500

    finished styling of login and password recovery page

--- front-end/src/BackHomeButton/BackHomeButtonBordered.js
+++ front-end/src/BackHomeButton/BackHomeButtonBordered.js
@@ -4,7 +4,14 @@ import { Link } from "react-router-dom";
 
 import './BackHomeButtonBordered.scss';
 
-export const BackHomeButtonBordered = ({ className }) => 
-<div className={classNames("back-home", className)}>
-    <Link to='/' className="back-home-button-bordered">Back Home</Link>
-</div>
+export const BackHomeButtonBordered = ({ className, noContainer = false }) => {
+    const Button = () => <Link to='/' className="back-home-button-bordered">Back Home</Link>
+
+    if (noContainer) return <Button />;
+
+    return (
+        <div className={classNames("back-home", className)}>
+            <Button />
+        </div>
+    )
+}
