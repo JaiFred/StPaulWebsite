7c5a4845c82f33a7f57c5567e9968efcf3771894
JaiVon <frederickjaivon@yahoo.com>
2023-01-23 19:30:52 -0500

    styling the profile page

--- front-end/src/ProfilePage.js
+++ front-end/src/ProfilePage.js
@@ -1,39 +1,47 @@
 //Hooks
 import { useState, useEffect } from "react";
-import { Link, Navigate } from 'react-router-dom'
+import { Link, Navigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-
 
 //components
 import SubscriptionCard from "./SubscriptionCard";
 import EditProfileModal from "./EditProfileModal";
 import AccountDeleteConfirmationModal from "./AccountDeleteConfirmationModal";
 
-function ProfilePage({ currentUser, setCurrentUser, authChecked, editProfileIsOpen, setEditProfileIsOpen, accountDeleteIsOpen, setAccountDeleteIsOpen }) {
+import './ProfilePage.scss'
 
-// const lastName = currentUser?.user?.last_name || currentUser?.last_name;
+function ProfilePage({
+  currentUser,
+  setCurrentUser,
+  authChecked,
+  editProfileIsOpen,
+  setEditProfileIsOpen,
+  accountDeleteIsOpen,
+  setAccountDeleteIsOpen,
+}) {
+  // const lastName = currentUser?.user?.last_name || currentUser?.last_name;
 
-let navigate = useNavigate();
+  let navigate = useNavigate();
 
-console.log(`currentUser: ${JSON.stringify(currentUser)}`);
-console.log(`currentUser?.user: ${JSON.stringify(currentUser?.user)}`);
+  console.log(`currentUser: ${JSON.stringify(currentUser)}`);
+  console.log(`currentUser?.user: ${JSON.stringify(currentUser?.user)}`);
 
-const profileFirstName = currentUser?.first_name || currentUser?.user.first_name;
-const profileLastName = currentUser?.last_name || currentUser?.user.last_name;
-const profileEmail = currentUser?.email || currentUser?.user.email;
+  const profileFirstName =
+    currentUser?.first_name || currentUser?.user.first_name;
+  const profileLastName = currentUser?.last_name || currentUser?.user.last_name;
+  const profileEmail = currentUser?.email || currentUser?.user.email;
 
-const userId = currentUser?.id || currentUser?.user?.id;
+  const userId = currentUser?.id || currentUser?.user?.id;
 
+  const [email, setEmail] = useState(profileEmail);
+  const [firstName, setFirstName] = useState(profileFirstName);
+  const [lastName, setLastName] = useState(profileLastName);
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [errors, setErrors] = useState([]);
 
-const [ email, setEmail ] = useState(profileEmail);
-const [ firstName, setFirstName ] = useState(profileFirstName);
-const [ lastName, setLastName ] = useState(profileLastName);
-const [ password, setPassword ] = useState("");
-const [ error, setError ] = useState ("");
-const [errors, setErrors] = useState([]);
-
-function handleUserDelete (event) {
+  function handleUserDelete(event) {
     event.preventDefault();
     fetch(`/api/users/${userId}`, {
       method: "DELETE",
@@ -50,16 +58,16 @@ function handleUserDelete (event) {
           setCurrentUser(null);
           localStorage.setItem("currentUserId", null);
           navigate("/login");
-          setAccountDeleteIsOpen(false) //new
+          setAccountDeleteIsOpen(false); //new
         });
-        } else {
-            console.log('Wrong credentials!');
-            setError("Wrong username/password!");
-        }  
+      } else {
+        console.log("Wrong credentials!");
+        setError("Wrong username/password!");
+      }
     });
-  };
+  }
 
-  function handleUserEdit (event) {
+  function handleUserEdit(event) {
     event.preventDefault();
     fetch(`/api/users/${userId}`, {
       method: "PATCH",
@@ -67,9 +75,9 @@ function handleUserDelete (event) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        email: email,        
+        email: email,
         first_name: firstName,
-        last_name: lastName
+        last_name: lastName,
       }),
     }).then((response) => {
       if (response.ok) {
@@ -80,74 +88,90 @@ function handleUserDelete (event) {
 
           if (profileEmail !== email) {
             setCurrentUser(null);
-            navigate('/update_email_success');
+            navigate("/update_email_success");
           }
-
         });
-      }
-      else {
-        console.log('server response');
+      } else {
+        console.log("server response");
         response.json().then((error) => {
           console.log(`message: ${JSON.stringify(error)}`);
-          setErrors(error.message)
+          setErrors(error.message);
         });
-      }  
+      }
     });
-  };
-
-  if (!authChecked){
-      return(
-        <div></div>
-      )
   }
-    
-  return(
-    <div className='profile-page-background'>
-        <div className='profile-info-square'>
-            <h1 className='profile-page-title'>Your Profile</h1>
-            <h3>Name: {profileFirstName} {profileLastName}</h3>
-            <h3>email: {profileEmail}</h3>
-                    
-            <button className='change-account-info-modal-btn' type='button' onClick={() => setEditProfileIsOpen(true)}>Edit account info</button>
-                <EditProfileModal 
-                  currentUser={currentUser} 
-                  editProfileIsOpen={editProfileIsOpen} 
-                  setEditProfileIsOpen={setEditProfileIsOpen}
-                  handleUserEdit={handleUserEdit}
-                  firstName={firstName}
-                  setFirstName={setFirstName}
-                  lastName={lastName}
-                  setLastName={setLastName}
-                  email={email}
-                  setEmail={setEmail}
-                  errors={errors}
-                />
 
-            <h3>Subscriptions</h3>
-            <div>
-                <Link to='/subscriptions_page'>See your offering subscriptions</Link>
-            </div>
-            <div>
-                <Link to='/'>Back Home</Link>
-            </div>
+  if (!authChecked) {
+    return <div></div>;
+  }
 
-            <div>
-            <button className='account-delete-modal-btn' type='button' onClick={() => setAccountDeleteIsOpen(true)}>Delete account</button>
-                <AccountDeleteConfirmationModal 
-                    currentUser={currentUser} 
-                    accountDeleteIsOpen={accountDeleteIsOpen} 
-                    setAccountDeleteIsOpen={setAccountDeleteIsOpen} 
-                    handleUserDelete={handleUserDelete}
-                    email={email}
-                    setEmail={setEmail}
-                    password={password}
-                    setPassword={setPassword}
-                    error={error}
-                />
-            </div>
+  return (
+    <div className="profile-page-background">
+      <div className="profile-info-square">
+        <h3 className="profile-info-name">{profileFirstName} {profileLastName}</h3>
+        
+        <h1 className="profile-page-title">Your Profile</h1>
+        
+        {/* Buttons */}
+        <button
+            className="account-delete-modal-btn"
+            type="button"
+            onClick={() => setAccountDeleteIsOpen(true)}
+          >
+            Delete account
+        </button>
+        <button
+          className="change-account-info-modal-btn"
+          type="button"
+          onClick={() => setEditProfileIsOpen(true)}
+        >
+          Edit account info
+        </button>
+
+      {/* Email */}
+        <div>
+          <strong>Your email:</strong><br />
+          {profileEmail}
         </div>
+
+        <EditProfileModal
+          currentUser={currentUser}
+          editProfileIsOpen={editProfileIsOpen}
+          setEditProfileIsOpen={setEditProfileIsOpen}
+          handleUserEdit={handleUserEdit}
+          firstName={firstName}
+          setFirstName={setFirstName}
+          lastName={lastName}
+          setLastName={setLastName}
+          email={email}
+          setEmail={setEmail}
+          errors={errors}
+        />
+
+        <h3>Subscriptions</h3>
+        <div>
+          <Link to="/subscriptions_page">See your offering subscriptions</Link>
+        </div>
+        <div>
+          <Link to="/">Back Home</Link>
+        </div>
+
+        <div>
+          <AccountDeleteConfirmationModal
+            currentUser={currentUser}
+            accountDeleteIsOpen={accountDeleteIsOpen}
+            setAccountDeleteIsOpen={setAccountDeleteIsOpen}
+            handleUserDelete={handleUserDelete}
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            error={error}
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
