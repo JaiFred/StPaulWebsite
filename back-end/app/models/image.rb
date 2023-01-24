d0b77ec15de8069841584f6ffe355016f8aef2c4
JaiVon <frederickjaivon@yahoo.com>
2023-01-24 11:31:16 -0500

    WIP: image handler for react quill

--- back-end/app/models/image.rb
+++ back-end/app/models/image.rb
@@ -1,3 +1,7 @@
 class Image < ApplicationRecord
     has_one_attached :file
+    # TOOD: image processing and resize
+    # Options: See if Active Storage helps with hte image resize OR 
+    # whether we have to use Shrine gem
+    # 
 end
--- front-end/src/Editor/Editor.js
+++ front-end/src/Editor/Editor.js
@@ -4,73 +4,78 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 const Editor = ({ id, value, onChange, placeholder = "", heading }) => {
-  const editorRef = useRef(null);
+  // const editorRef = useRef(null);
 
   useEffect(() => {
       document.querySelectorAll('[role="dialog"]').forEach(modal =>
-          modal.removeAttribute('tabindex')    
+          modal.removeAttribute('tabindex')
       )
   }, [])
 
-  function image_upload_handler (blobInfo, success, failure, progress) {
-    console.log('inside image_upload_handler!!!!')
-    var xhr, formData;
-  
-    xhr = new XMLHttpRequest();
-    xhr.withCredentials = false;
-    xhr.open('POST', '/api/images');
-  
-    xhr.upload.onprogress = function (e) {
-      progress(e.loaded / e.total * 100);
-    };
-  
-    xhr.onload = function() {
-      var json;
-  
-      if (xhr.status === 403) {
-        failure('HTTP Error: ' + xhr.status, { remove: true });
-        return;
-      }
-  
-      if (xhr.status < 200 || xhr.status >= 300) {
-        failure('HTTP Error: ' + xhr.status);
-        return;
-      }
-  
-      json = JSON.parse(xhr.responseText);
-  
-      if (!json || typeof json.location != 'string') {
-        failure('Invalid JSON: ' + xhr.responseText);
-        return;
-      }
-  
-      success(json.location);
-    };
-  
-    xhr.onerror = function () {
-      failure('Image upload failed due to a XHR Transport error. Code: ' + xhr.status);
-    };
-  
-    formData = new FormData();
-    formData.append('file', blobInfo.blob(), blobInfo.filename());
-  
-    xhr.send(formData);
-  };
-  // );
-  
+  function uploadFiles(uploadFileObj, filename, quillObj) {
+    var apiUrl = `/api/images`;
 
-  console.log(`process.env.REACT_APP_TINY_KEY: ${process.env.REACT_APP_TINY_KEY}`);
+    try {
+      if (uploadFileObj != '') {
+        fetch(apiUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;odata=verbose'
+          },
+          body: uploadFileObj // This is your file object
+        }).then((response) => {
+          console.log(response);
+          const range = quillObj.getEditorSelection();
+
+          // quillObj.getEditor().insertEmbed(range.index, 'image', res);
+        }).catch((error) =>
+          console.log(error)
+        );
+      }
+    }
+    catch (error) {
+      console.log('uploadFiles : ' + error);
+    }
+  }
+
+  function imageHandler() {
+    console.log("Uploading image handler....")
+    const input = document.createElement('input');
+
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+
+    input.onchange = () => {
+      var file = input.files[0];
+      var formData = new FormData();
+
+      formData.append('image', file);
+
+      var fileName = file.name;
+
+      console.log(`fileName: ${fileName}`);
+
+      const res = uploadFiles(file, fileName, quillObj);
+      console.log(`res: ${JSON.stringify(res)}`);
+    };
+  }
 
   const modules = {
-    toolbar: [
-      [{ 'header': [1, 2, false] }],
-      ['bold', 'italic', 'underline','strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image', 'video'],
-      ['clean']
-    ],
+    toolbar: {
+      container: [
+        [{ 'header': [1, 2, false] }],
+        ['bold', 'italic', 'underline','strike', 'blockquote'],
+        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+        ['link', 'image', 'video'],
+        ['clean']
+      ],
+      handlers: {        
+        'image': imageHandler
+      }
+    }
   }
-  
+
   const formats = [
     'header',
     'bold', 'italic', 'underline', 'strike', 'blockquote',
@@ -78,37 +83,22 @@ const Editor = ({ id, value, onChange, placeholder = "", heading }) => {
     'link', 'image', 'video'
   ]
 
+  var quillObj;
+
   return (
     <div className="editor-wrapper">
       {heading && <h2>{heading}</h2>}
 
-      <ReactQuill 
+      <ReactQuill
+       ref={(el) => {  
+        quillObj = el;  
+        }}
         theme="snow"
         modules={modules}
         formats={formats}
-        value={value} 
+        value={value}
         onChange={onChange}
       />;
-
-      {/* <TinyMCE
-        id={id}
-        apiKey={process.env.REACT_APP_TINY_KEY}
-        onEditorChange={onChange}
-        onInit={(evt, editor) => (editorRef.current = editor)}
-        value={value}
-        imagesUploadUrl="http://localhost:3000/api/images"
-        init={{
-          selector: 'textarea',
-          // images_upload_handler: image_upload_handler          
-          //automatic_uploads: true,
-          image_title: true,
-          height: 500,
-          menubar: 'file edit view insert format tools table tc help',
-          plugins: 'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker imagetools textpattern noneditable help formatpainter permanentpen pageembed charmap mentions quickbars linkchecker emoticons advtable export'.split(' '),
-          toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-        }}
-      {/* /> */}
     </div>
   );
 };
