0c2d47bd2e6b3209036df6632b89d58c96edabcb
JaiVon <frederickjaivon@yahoo.com>
2023-01-25 20:41:51 -0500

    styling subscription page - need to make responsive to small screens. Styled account edit modal and signup success page

--- front-end/src/EditProfileModal.js
+++ front-end/src/EditProfileModal.js
@@ -4,26 +4,28 @@ import { Button } from "react-bootstrap";
 
 //Components
 import EditProfile from "./EditProfile"
+import { DarkHeader } from './Modal/Header';
 
 
 function EditProfileModal({ errors, currentUser, editProfileIsOpen, setEditProfileIsOpen, handleUserEdit, firstName, setFirstName, lastName, setLastName, email, setEmail }){
 
     console.log(`firstName 1: ${firstName}`);
+    const onCancel = () => setEditProfileIsOpen(false)
 
     return(
-        <div>
-            <Modal className='modal'
-                show={ editProfileIsOpen }
-            >
-            <ModalHeader>
-            <ModalTitle>Saint Paul Baptist Church</ModalTitle>
-            <ModalTitle>Edit:</ModalTitle>
+        <Modal
+            className='modal'
+            show={ editProfileIsOpen }
+        >
+            {/* <ModalHeader>
+                <ModalTitle>Saint Paul Baptist Church</ModalTitle>
+                <button type="button" onClick={() => {setEditProfileIsOpen(false)}}>X</button>
+            </ModalHeader> */}
+            <DarkHeader onCancel={onCancel} />
             {/* <ModalTitle> <button type="button" onClick={() => {resetForm()}}>X</button></ModalTitle> */}
-            <ModalTitle> <button type="button" onClick={() => {setEditProfileIsOpen(false)}}>X</button></ModalTitle> 
-                <ModalBody>
-                </ModalBody>
-            </ModalHeader>
-            <ModalFooter>
+            
+            <ModalBody className="text-center ">
+                <h2 className="modal-body-heading">Edit Account Info</h2>
                 <EditProfile 
                     currentUser={currentUser} 
                     handleUserEdit={handleUserEdit}
@@ -35,11 +37,13 @@ function EditProfileModal({ errors, currentUser, editProfileIsOpen, setEditProfi
                     setEmail={setEmail}
                     errors={errors}
                 />
-                <button type="button" onClick={() => {setEditProfileIsOpen(false)}}>Cancel</button>
+            </ModalBody>
+            
+            <ModalFooter className="grid-2-columns flex-grow-1 border-0 modal-footer-buttons">
+                <Button variant="primary" type="button" onClick={handleUserEdit} className="submit">Update Profile</Button>
+                <button type="button" onClick={onCancel} className="cancel">Cancel</button>
             </ModalFooter>
-
-            </Modal>
-        </div>
+        </Modal>
     )
 }
 
