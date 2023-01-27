bc0eee035bded1ed569df84cb669dfce60afdfdf
JaiVon <frederickjaivon@yahoo.com>
2023-01-27 17:52:09 -0500

    finished styling for delete account modal and improved screen responsiveness for subscription page

--- front-end/src/AccountDeleteConfirmationModal.js
+++ front-end/src/AccountDeleteConfirmationModal.js
@@ -1,7 +1,7 @@
 //Hooks
 import { Link } from 'react-router-dom'
 import { Modal, ModalHeader, ModalBody, ModalFooter, ModalTitle, Button } from 'react-bootstrap'
-
+import { DarkHeader } from './Modal/Header'
 import DeleteProfile from './DeleteProfile'
 
 function AccountDeleteConfirmationModal({ currentUser, accountDeleteIsOpen, setAccountDeleteIsOpen, handleUserDelete, email, setEmail, password, setPassword, error }){
@@ -11,32 +11,20 @@ function AccountDeleteConfirmationModal({ currentUser, accountDeleteIsOpen, setA
              <Modal className='modal'
                 show={ accountDeleteIsOpen }
              >
-            <ModalHeader >
-                <ModalTitle>Saint Paul Baptist Church</ModalTitle>
-                <ModalTitle><button type="button" onClick={() => {setAccountDeleteIsOpen(false)}}>X</button></ModalTitle> 
-            </ModalHeader>
-            <ModalHeader >
-                    <ModalTitle>Do you wish to delete your account</ModalTitle>
-                </ModalHeader>
-                    <ModalFooter> 
-                        {/* <Link to='/'>
-                        <button type="button" onClick={() => {setAccountDeleteIsOpen(false)}}>yes</button>
-                        </Link> */}
-                        <DeleteProfile 
-                            handleUserDelete={handleUserDelete} 
-                            email={email}
-                            setEmail={setEmail}
-                            password={password}
-                            setPassword={setPassword}
-                            error={error}
-                            setAccountDeleteIsOpen={setAccountDeleteIsOpen}
-                        />
-                        <button type="button" onClick={() => {setAccountDeleteIsOpen(false)}}>No</button> 
-                    </ModalFooter> 
-
+                <DarkHeader onCancel={() => setAccountDeleteIsOpen(false)} />
+                <ModalBody>
+                    <DeleteProfile 
+                        handleUserDelete={handleUserDelete} 
+                        email={email}
+                        setEmail={setEmail}
+                        password={password}
+                        setPassword={setPassword}
+                        error={error}
+                        setAccountDeleteIsOpen={setAccountDeleteIsOpen}
+                    />
+                </ModalBody>
              </Modal>
         </div>
-
     )
 }
 
