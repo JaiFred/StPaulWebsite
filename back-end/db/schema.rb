f9dceee8041fba3b09b56e1efd128576d81f1adb
JaiFred <105246008+JaiFred@users.noreply.github.com>
2023-01-28 11:47:38 -0500

    Migrate to Postgre SQL from SQLite3 (#4)

--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_01_20_020916) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -53,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_20_020916) do
 
   create_table "documents", force: :cascade do |t|
     t.text "description"
-    t.integer "honor_page_id", null: false
+    t.bigint "honor_page_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["honor_page_id"], name: "index_documents_on_honor_page_id"
@@ -77,7 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_20_020916) do
   create_table "future_subscriptions", force: :cascade do |t|
     t.bigint "billing_cycle_anchor"
     t.date "payment_start_date"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "plan"
     t.string "title"
     t.integer "frequency"
@@ -98,7 +101,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_20_020916) do
   end
 
   create_table "subscriptions", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "stripe_subscription_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -1,25 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-# create_table :events do |t|
-#     t.string :title
-#     t.datetime :starts
-#     t.datetime :ends
-#     t.string :details
-#     t.string :location
-
-# "%A, %d %b %Y %l:%M %p"
-
-a = Time.now
-
-puts "Time a strftime form : #{Time.now.strftime("at %I:%M %p")}\n\n"
-
-DashboardDocument.create(description: 'Hello world!')
+DashboardDocument.create(description: 'New Document')
 
 Event.destroy_all
 
@@ -30,14 +9,4 @@ e4 =  Event.create!(title: "Test3", starts: DateTime.new(2022, 05, 10, 10, 00, 0
 e5 =  Event.create!(title: "Test4", starts: DateTime.new(2022, 01, 10, 10, 00, 0), ends: DateTime.new(2022, 12, 10, 14, 00, 0), details: "", address_line_1: "tests4", address_line_2: "", city:"", state_province_region:"", zip_postalcode: "", zip_postalcode: "", country: "t" )
 e6 =  Event.create!(title: "Test5", starts: DateTime.new(2022, 05, 10, 10, 00, 0), ends: DateTime.new(2022, 05, 10, 14, 00, 0), details: "tests3", address_line_1: "tests3", address_line_2: "", city:"", state_province_region:"", zip_postalcode: "", zip_postalcode: "", country: "" )
 
-# I recieved an octal numbers problem/error when I switched e4's day numbers to 09
-
-
-
-puts "#{Time.new(2022, 06, 12, 9, 00, 0).strftime("%B %d %Y, at %-I:%M%p")}"
-
-# t.string :first_name
-#       t.string :last_name
-#       t.string :username
-#       t.string :email
-#       t.string :password
+puts "Done seeding!"
