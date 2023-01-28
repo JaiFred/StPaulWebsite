0c953f7943b9ac515dfccde7570b2a4510e69183
JaiVon <frederickjaivon@yahoo.com>
2023-01-28 10:59:15 -0500

    cleanusp

--- back-end/app/models/image.rb
+++ back-end/app/models/image.rb
@@ -1,7 +1,3 @@
 class Image < ApplicationRecord
     has_one_attached :file
-    # TOOD: image processing and resize
-    # Options: See if Active Storage helps with hte image resize OR 
-    # whether we have to use Shrine gem
-    # 
 end
