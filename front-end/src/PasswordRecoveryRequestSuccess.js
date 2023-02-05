909a85c869a10bdd08b6bf7ff3acbc2f3ccec2fc
JaiVon <frederickjaivon@yahoo.com>
2023-02-05 14:33:25 -0500

    added styles to password recovery success page

--- front-end/src/PasswordRecoveryRequestSuccess.js
+++ front-end/src/PasswordRecoveryRequestSuccess.js
@@ -9,12 +9,12 @@ import Church from './images/StPaulBaptistChurch.JPG'
 function PasswordRecoveryRequestSuccess(){
 
     return(
-        <div>
+        <div className="success-page-wrapper" style={{ backgroundImage: `url(${Church})`}}>
+            <div className="success-page-message">
             <h1>We have recieved your request to change your password</h1>
             <p>please check your email and follow the links to change your password</p>
-            <div>
-            <Link to='/login' className="main">back to login</Link>
             </div>
+            <Link to='/login' className="back-home-button">back to login</Link>
         </div>
     )
 }
