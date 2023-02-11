08a9715adedddffb7c993bc9eb9f2a6a2b8df4ff
JaiVon <frederickjaivon@yahoo.com>
2023-02-11 10:18:18 -0500

    Fallback controller to make React router t owork

--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,8 @@ Rails.application.routes.draw do
     resources :documents, only: %i[show update destroy]
   end
 
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
