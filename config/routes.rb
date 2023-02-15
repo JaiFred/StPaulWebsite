f6195ec02a54e932b8c3003ddd4a1b85e159fdd7
JaiVon <frederickjaivon@yahoo.com>
2023-02-14 21:53:50 -0500

    Fix the login url in confirmation show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {
-                                 sign_in: 'login',
-                                 sign_out: 'logout',
-                                 registration: 'signup'
-                               },
+  devise_for :users, path: '',
                      controllers: {
                        sessions: 'users/sessions',
                        registrations: 'users/registrations',
