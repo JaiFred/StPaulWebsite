29fc12af31311a8c57055ed0568fe3e19b4b2ce3
JaiVon <frederickjaivon@yahoo.com>
2023-02-14 22:17:55 -0500

    Bring back devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, path: '',
+  devise_for :users, path: '', path_names: {
+                                 sign_in: 'signin',
+                                 sign_out: 'logout',
+                                 registration: 'signup'
+                               },
                      controllers: {
                        sessions: 'users/sessions',
                        registrations: 'users/registrations',
