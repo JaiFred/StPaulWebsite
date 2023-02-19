f1cf75d27223c110f4ffc11594112821a45d5802
JaiVon <frederickjaivon@yahoo.com>
2023-02-19 11:49:27 -0500

    Fix devise login url

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {
-                                 sign_in: 'signin',
+                                 sign_in: 'login',
                                  sign_out: 'logout',
                                  registration: 'signup'
                                },
