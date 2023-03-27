6ffd4e249f70612aee0b40e873b214179bd6eb02
JaiVon <frederickjaivon@yahoo.com>
2023-03-27 18:15:32 -0400

    Open admin link in a new tab

--- client/src/ProfilePage.js
+++ client/src/ProfilePage.js
@@ -144,7 +144,7 @@ function ProfilePage({
           <div>
             <h3>Admin controls</h3>
             <div className="profile-info-action-buttons mb-5">
-              <a href={ADMIN_LOGIN_ENDPOINT}>Admin panel for managing data</a>
+              <a href={ADMIN_LOGIN_ENDPOINT} target="_blank">Admin panel for managing data</a>
             </div>
           </div>
         )}
