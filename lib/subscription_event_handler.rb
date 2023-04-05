0b9a56caa4c4604db90043f894d769297092b238
JaiVon <frederickjaivon@yahoo.com>
2023-04-05 19:11:13 -0400

    Finished stripe webhooks implementation for cancelled subscription

--- lib/subscription_event_handler.rb
+++ lib/subscription_event_handler.rb
@@ -1,19 +1,17 @@
 class SubscriptionEventHandler
   def call(event)
-    puts "event.type: #{event.type}"
+    # puts "event.type: #{event.type}"
     begin
       method = "handle_" + event.type.tr('.', '_')
-      puts "method: #{method}"
+      # puts "method: #{method}"
       self.send method, event
-    # rescue JSON::ParserError => e
-    #   # handle the json parsing error here
-    #   raise # re-raise the exception to return a 500 error to stripe
-    # rescue NoMethodError => e
-    #   #code to run when handling an unknown event
     end
   end
 
   def handle_customer_subscription_deleted(event)
-    binding.pry
+    sub_id = event.data.object.id
+    canceled_at = Time.at event.data.object.canceled_at
+    subscription = Subscription.find_by(stripe_subscription_id: sub_id)
+    subscription.update!(cancelled_at: canceled_at)
   end
 end
