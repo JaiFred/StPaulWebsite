4a2402fef735982a65a3b640fabc34d0d55fac0d
JaiVon <frederickjaivon@yahoo.com>
2023-04-09 15:06:00 -0400

    cleanups

--- config/initializers/stripe_events.rb
+++ config/initializers/stripe_events.rb
@@ -1,6 +1,5 @@
 require 'subscription_event_handler.rb'
 
-# Stripe.api_key = Rails.application.credentials.stripe[Rails.env.to_sym][:publishable_key]
 StripeEvent.signing_secret = ENV.fetch('STRIPE_SIGNING_SECRET', nil)
 Stripe.api_key = ENV.fetch('STRIPE_SECRET_KEY', nil)
 
