b9bdc4681a2f4114176666defc51f38a7159ad0f
JaiVon <frederickjaivon@yahoo.com>
2023-04-25 23:50:53 -0400

    add TODO comment

--- app/sidekiq/delete_s3_images_job.rb
+++ app/sidekiq/delete_s3_images_job.rb
@@ -1,5 +1,6 @@
 class DeleteS3ImagesJob
   include Sidekiq::Job
+  include Rails.application.routes.url_helpers
 
   def perform
     # new table: rails_representation_url, resize, object.file referecne, object.file aws s3 reference
@@ -11,12 +12,19 @@ class DeleteS3ImagesJob
     # resize_values = ['485x485', '700x700']
     # end
     # generate url for both resize_values from the array ^^
-    # image_url = Rails.application.routes.url_helpers.rails_representation_url(image.file.variant(resize: resize_value))
+    
+    # image_url = Rails.application.routes.default_url_options[:host] + Rails.application.routes.url_helpers.rails_representation_url(
+    #   image.file.variant(resize: resize_value).processed, only_path: true
+    # )
 
+    # 0. FIRST THING TOMORROW: Verify if we can get rid of ImageDatum model altogether.
+    # and, just use image_url to determine whether it's a valid one or not. If invalid, then, destroy the image.
+    # 
     # next unless ImageDatum.exists?(image_id: image.id, image_url: image_url)
 
-    # Document.description && Dashboard.description parsing will give array of valid_image_urls
+    # 1. Document.description && DashboardDocument.description parsing will give array of valid_image_urls
 
+    # 2.
     # unless valid_image_urls.include?(image_url) # 2nd check before destroying
     # image.destroy!
     # end
