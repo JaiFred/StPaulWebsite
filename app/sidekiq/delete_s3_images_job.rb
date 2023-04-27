7e77eb83d26f23f9099b1f54ac88f0d20493eb25
JaiVon <frederickjaivon@yahoo.com>
2023-04-27 01:07:13 -0400

    DeleteS3ImagesJob algorithm is completed.

--- app/sidekiq/delete_s3_images_job.rb
+++ app/sidekiq/delete_s3_images_job.rb
@@ -1,33 +1,64 @@
+require 'nokogiri'
+
 class DeleteS3ImagesJob
   include Sidekiq::Job
   include Rails.application.routes.url_helpers
 
-  def perform
-    # new table: rails_representation_url, resize, object.file referecne, object.file aws s3 reference
-    # write a rake task, where we fetch all the existing aws s3 objects and build an array,
-    # fetch all the img url by parsing all the document text description parsing
+  def perform      
+    valid_image_urls = []
+    (Document.pluck(:description) + DashboardDocument.pluck(:description)).each do |document|
+      html = Nokogiri::HTML(document)      
+  
+      html.xpath("//img").each do |row|
+        # puts row.inspect
+        valid_image_urls << row['src']
+      end
+    end
 
-    # valid_urls = fetch all the img url by parsing all the document text description parsing
-    # all_image_urls = Image.all.map do |image|
-    # resize_values = ['485x485', '700x700']
-    # end
-    # generate url for both resize_values from the array ^^
-    
-    # image_url = Rails.application.routes.default_url_options[:host] + Rails.application.routes.url_helpers.rails_representation_url(
-    #   image.file.variant(resize: resize_value).processed, only_path: true
-    # )
+    puts "*" * 100
+    puts "valid_image_urls: #{valid_image_urls.inspect}"
+    puts "valid_image_urls size: #{valid_image_urls.size}"
+    puts "*" * 100
 
-    # 0. FIRST THING TOMORROW: Verify if we can get rid of ImageDatum model altogether.
-    # and, just use image_url to determine whether it's a valid one or not. If invalid, then, destroy the image.
-    # 
-    # next unless ImageDatum.exists?(image_id: image.id, image_url: image_url)
+    puts "Processing total images: #{Image.count}"
 
-    # 1. Document.description && DashboardDocument.description parsing will give array of valid_image_urls
+    Image.all.each do |image|
+      puts "processing image: #{image.id}"
 
-    # 2.
-    # unless valid_image_urls.include?(image_url) # 2nd check before destroying
-    # image.destroy!
-    # end
-    # end
+      if image.file.blank?
+        puts "no file or variant present for image: #{image.id}. so, deleting it."
+        image.destroy!
+        next
+      end
+
+      image_url_1 = Rails.application.routes.default_url_options[:host] + Rails.application.routes.url_helpers.rails_representation_url(
+        image.file.variant(resize: '485x485')&.processed, only_path: true
+      )
+      image_url_2 = Rails.application.routes.default_url_options[:host] + Rails.application.routes.url_helpers.rails_representation_url(
+        image.file.variant(resize: '700x700')&.processed, only_path: true
+      )
+
+      puts "#" * 100      
+      puts "image_url_1: #{image_url_1.inspect}"
+      puts "image_url_2: #{image_url_2.inspect}"
+      puts "#" * 100
+
+      if valid_image_urls.include?(image_url_1)
+        puts "image_url_1 is present in the valid_image_urls. so keeping the image: #{image.id}"
+        next
+      end
+
+      if valid_image_urls.include?(image_url_2)
+        puts "image_url_2 is present in the valid_image_urls. so keeping the image: #{image.id}"
+        next
+      end
+
+      puts "image_url_1 and image_url_2 are not found in the valid_image_urls. So, deleting the image: #{image.id}"
+
+      image.destroy!
+    rescue Exception => ex
+      puts "Image: #{image.id} got exception #{ex.message} while processing. So, deleting it."
+      image.destroy!
+    end
   end
 end
---
+++ client/src/Inputs/Events/EventCard.js
@@ -0,0 +1,99 @@
+//Hooks
+import React, { useState, useEffect } from "react";
+import { useNavigate, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+// import { useParams } from 'react-router-dom';
+
+//Components
+import { EventCardControls } from "../EventCardControls";
+
+import './EventCard.scss'
+
+
+// Goal:
+// Have each event display with only the day number, month and year on the EventCard page
+// Have each event display with the day, day number, month, year and time on the EventInfoPage
+// Have a months filter bar that appears when an event within that month is made - you can click on months to show events that take place during the month
+
+
+function EventCard({event, currentUser, handleDeleteEvent, handleEditEvent}){
+
+    const [ deleteIsOpen , setDeleteIsOpen ] = useState(false);
+    const [ editEventIsOpen , setEditEventIsOpen ] = useState(false);
+
+    //edit
+
+    const { id, title, starts_short, ends_short, details, location, image, starts_raw } = event;
+
+    // console.log(event);
+
+
+    const navigate = useNavigate()
+
+    function handleDeleteClick(e) {
+        e.preventDefault()
+        const reqObj = {
+            method: "DELETE"
+        }
+        fetch(`/api/events/${id}`, reqObj )
+        .then((res) => res.json())
+        .then(handleDeleteEvent(id))
+    }
+
+    const imgSrc = image || "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
+
+    return <div className="event-card">
+        <div className="event-card-outer">
+            <div className="event-card-inner">
+                <Link to={`/events/${event.id}`} className="event-card-link">
+                    <div className="event-card-infos">
+                        <div className="event-card-infos-image" style={{backgroundImage: `url(${imgSrc})`}}></div>
+                        <div className="event-card-infos-inner__wrapper">
+                            <div className="event-card-infos-inner">
+                                <h2 className="event-card-title">{title}</h2>
+                                <p className="event-card-date">{starts_short}&#160;&#160; - &#160;&#160;{ends_short}</p>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+                <EventCardControls
+                    currentUser={currentUser}
+                    event={event}
+                    deleteIsOpen={deleteIsOpen}
+                    setDeleteIsOpen={setDeleteIsOpen}
+                    handleDeleteClick={handleDeleteClick}
+                    editEventIsOpen={editEventIsOpen}
+                    setEditEventIsOpen={setEditEventIsOpen}
+                    handleEditEvent={handleEditEvent}
+                />
+            </div>
+        </div>
+    </div>
+}
+
+export default EventCard;
+
+
+
+
+
+
+
+//  <div>
+//                         <Link to={`/events/${event.id}`}>
+//                         <button className="event-card">
+//                             <div>
+//                                 <h2>{title}</h2>
+//                                 {
+//                                     image ? <img src={image} width="500" height="600"></img> : <img src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=" width="500" height="600"></img>
+//                                 }
+//                                 <h2>starts: {starts_short} - ends: {ends_short}</h2>
+//                                 <Routes>
+//                                     <Route path="/events/:id" element={<EventInfoPage currentUser={currentUser} event={event} events={events} setEvents={setEvents}/>}/>
+//                                 </Routes>
+//                                <label htmlFor="more-info">Click for More Info</label>
+//                             </div>
+//                         </button>
+//                         </Link>
+//                     </div>
+//                 )}
